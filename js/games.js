6fe8489f7e45f31d0ee6922693fe301d5c1909f4
Thiago S. Meirelles <thiagomeirelles11037@gmail.com>
2023-06-23 18:17:35 -0300

    Olá

---
+++ js/games.js
@@ -0,0 +1,18 @@
+function darkMode() {
+    var element = document.body;
+    var content = document.getElementById("DarkModetext");
+    element.className = "dark-mode";
+    var element = document.getElementById("main-sec");
+    element.classList.add("dark-mode");
+    element.classList.remove("light-mode");
+
+  }
+  
+  function lightMode() {
+    var element = document.body;
+    var content = document.getElementById("DarkModetext");
+    element.className = "light-mode";
+    var element = document.getElementById("main-sec");
+    element.classList.add("light-mode");
+    element.classList.remove("dark-mode");  
+  }
